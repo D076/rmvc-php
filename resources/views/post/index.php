e5b47c21c5b5fa9273b509e42e00c00015554284
Dmitry Titov <piratik44@mail.ru>
2023-01-07 16:17:53 +0700

    Init

---
+++ resources/views/post/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+Index page
+
+<div>
+    Store
+    <div>
+        <form action="/posts" method="post">
+            <input type="text" placeholder="value" name="title">
+            <input type="submit">
+        </form>
+    </div>
+</div>
+
+<div>
+    Title
+    <div>
+        <?php
+        if (isset($_SESSION['message'])) {
+            echo $_SESSION['message'];
+        }
+        ?>
+    </div>
+</div>
+</body>
+</html>
